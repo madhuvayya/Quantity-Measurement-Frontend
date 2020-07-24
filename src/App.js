193095d1968f12e78d745ba197915d99e3f37bde
madhuvayya <madhu.vayya@gmail.com>
2020-07-24 21:53:27 +0530

    Added Greeting component to display welcome message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Greeting from './components/Greeting'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Greeting />
     </div>
   );
 }
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Greeting extends Component {
+    render() {
+        return <div className="greeting">
+               <p>Welcome To Quantity Measurement</p>
+        </div>
+    }
+}
+
+export default Greeting;
