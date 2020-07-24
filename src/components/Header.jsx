60e72f0ea7fb7dd86cb4668d9aef06a79dbcb950
madhuvayya <madhu.vayya@gmail.com>
2020-07-24 19:11:46 +0530

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div class="header">
+                <div>Quanment</div>
+                <div>History</div>
+            </div>
+        ) 
+    }
+}
+
+export default Header;
