a6c0bcf454686528de3b91585bb7955b45acac77
madhuvayya <madhu.vayya@gmail.com>
2020-07-24 21:06:47 +0530

    Renamed scss class names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div class="header">
-                <div>Quanment</div>
-                <div>History</div>
+            <div className="header">
+                <div className="quanment">Quanment</div>
+                <div className="history">History</div>
             </div>
         ) 
     }
