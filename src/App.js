7afc7c2509f13ed45ab34530205745509cb4b267
madhuvayya <madhu.vayya@gmail.com>
2020-07-24 22:30:08 +0530

    Added QuantitiesContainer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Greeting from './components/Greeting'
+import QuantitiesContainer from './components/QuantitiesContainer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Greeting />
+      <QuantitiesContainer />
     </div>
   );
 }
---
+++ src/components/QuantitiesContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class QuantitiesContainer extends Component { 
+    render() {
+        return (
+            <div className='sub-container'>
+                <div className='type'>
+                    <div>CHOOSE TYPE</div>
+                </div>
+                <div className='quantities' >
+                    unit
+                </div>
+            </div>
+        )
+    }
+}
+
+export default QuantitiesContainer;
